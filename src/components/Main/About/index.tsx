b86f89b634dd77008d27449dc794278b07966e1a
Alex <alexsandro091092@gmail.com>
2023-01-30 14:09:23 -0300

    Adding About

--- src/components/Main/About/index.tsx
+++ src/components/Main/About/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import './styles.css';
+import owner from '../../../images/profile.jpg';
 
 const About = () => {
 	return (
 		<div id='About' className='about'>
-			<div>Foto</div>
-			<div>Sobre Mim</div>
+			<div className='container-about'>
+				<h1>Lorem ipsum dolor sit amet</h1>
+				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcae</p>
+			</div>
+			<div className='container-photo'>
+				<img src={owner} alt='Foto do Welton' />
+			</div>
 		</div>
 	);
 };
