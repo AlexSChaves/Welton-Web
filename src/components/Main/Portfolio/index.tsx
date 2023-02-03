d1954c393584f779cec2622f4fd61d3b10f78c98
Alex <alexsandro091092@gmail.com>
2023-02-03 15:31:06 -0300

    Fixing Portfolio Images

--- src/components/Main/Portfolio/index.tsx
+++ src/components/Main/Portfolio/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './styles.css';
-import img01 from './img/DESTAQUE_CATAVENTO.png';
-import img02 from './img/DESTAQUE_LIFT.png';
-import img03 from './img/DESTAQUE_LUCIUS.png';
-import img04 from './img/DESTAQUE_SMMK.png';
+import img01 from './img/01.jpg';
+import img02 from './img/02.png';
+import img03 from './img/03.png';
+import img04 from './img/04.png';
+import img05 from './img/05.png';
+import img06 from './img/06.png';
+import img07 from './img/07.png';
+import img08 from './img/08.png';
+import img09 from './img/09.png';
+import img10 from './img/10.png';
+import img11 from './img/11.jpg';
+import img12 from './img/12.jpg';
+import img13 from './img/13.png';
+import img14 from './img/14.png';
 
 const Portfolio = () => {
 	return (
@@ -18,10 +28,48 @@ const Portfolio = () => {
 				</span>
 			</div>
 			<div className='portfolio-images'>
-				<img src={img01} alt='catavento' />
-				<img src={img02} alt='lift' />
-				<img src={img03} alt='lucius' />
-				<img src={img04} alt='smmk' />
+				<div className='item-galeria w-2 h-2'>
+					<img src={img01} alt='' />
+				</div>
+				<div className='item-galeria w-2 h-1'>
+					<img src={img02} alt='' />
+				</div>
+				<div className='item-galeria w-1 h-2'>
+					<img src={img03} alt='' />
+				</div>
+				<div className='item-galeria w-1 h-1'>
+					<img src={img04} alt='' />
+				</div>
+				<div className='item-galeria w-2 h-2'>
+					<img src={img05} alt='' />
+				</div>
+				{/* <div>
+					<img src={img06} alt='' />
+				</div> */}
+				<div className='item-galeria w-1 h-2'>
+					<img src={img07} alt='' />
+				</div>
+				<div className='item-galeria w-1 h-1'>
+					<img src={img08} alt='' />
+				</div>
+				{/* <div>
+					<img src={img09} alt='' />
+				</div> */}
+				{/* <div>
+					<img src={img10} alt='' />
+				</div> */}
+				<div className='item-galeria w-1 h-1'>
+					<img src={img11} alt='' />
+				</div>
+				<div className='item-galeria w-1 h-1'>
+					<img src={img12} alt='' />
+				</div>
+				{/* <div className="item-galeria w-1 h-1">
+					<img src={img13} alt='' />
+				</div> */}
+				{/* <div>
+					<img src={img14} alt='' />
+				</div> */}
 			</div>
 		</div>
 	);
